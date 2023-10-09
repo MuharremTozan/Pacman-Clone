d39fdca8570bb45659ed3fb673cf34fe91378bc5
Muharrem Tozan <muharremtozan@hotmail.com>
2023-10-09 20:08:40 +0300

    Input system edited

--- Pacman-Clone/Assets/Scripts/Pacman.cs
+++ Pacman-Clone/Assets/Scripts/Pacman.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pacman : MonoBehaviour
 {
-   
-  
+    private Rigidbody2D pacmanRigidbody;
+
+    private void Awake()
+    {
+        pacmanRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void PacmanMovement()
+    {
+        Debug.Log("Move!");
+        pacmanRigidbody.AddForce(Vector2.right * 5f , ForceMode2D.Impulse);
+    }
 }
